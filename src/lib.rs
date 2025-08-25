ec7060c95f8e57a4908a08fb6ffc154a36e3f34b
Diego Correa Tristain <algoritmia@labormedia.cl>
2025-08-25 01:22:23 +0000

    Defines the problem to be solved and its properties

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,56 @@
+//! Twisted ElGamal Non-Interactive Chaum/Pedersen protocol
+//! Consider cyclic Group (|G, +) of prime order q with generator G and H with fixed and secret domain separator (hidden dependence).
+//! For Key Generation x <-$ Z_q, Y = x*G,
+//! With message in scalar form m, it encodes M = m*G in G.
+//!
+//! Encryption is as follows:
+//! k <-$ Z_q, 
+//! C_1 = k*G, C_2 = M + k*Y
+//! Ciphertext CT = (C_1, C_2)
+//!
+//! Decryption:
+//! Compute S' = x*C_1 = x*k*G = k*x*G = k*Y
+//! C_2 - S' = M + k*Y - k*Y = M = m*G
+//! Then solves the discrete logarithm (theoretical) of M = m*G to recover m
+//!
+//! Goal:
+//! Given a Public Key, a Commitment and a Ciphertext prove:
+//! There exist scalars m, r and k such that:
+//! C_m = m*G + r*H, C_1 = k*G, C_2 = M + k*Y  // This statement hides M = m*G with domain separated H and randomly generated r, while encrypting the same original m of the message.
+//! 
+//! Statement and witnesses:
+//! Public: G, H, Y, C_m, C_1, C_2
+//! Witnesses: m, r, k in Z_q
+//!
+//! C_m = m*G + r*H
+//! C_1 = k*G
+//! C_2 = M + k*Y
+//!
+//! Multi-relation Sigma Protocol (P, V):
+//! Prover P chooses random alpha, beta, gamma <-$ Z_q
+//! and defines:
+//! T_1 = gamma*G
+//! T_2 = alpha*G + gamma*Y
+//! T_3 = alpha*G + beta*H
+//!
+//! Computes Fiat-Shamir challenge for the Non-Interactive construction:
+//! e = Hash(G, H, Y, C_m, C_1, C_2, T_1, T_2, T_3)
+//! 
+//! and responds with scalars:
+//! s_m = alpha + e*m, s_r = beta + e*r, s_k = gamma + e*k
+//! 
+//! Validator V checks:
+//! s_k*G =? T_1 + e*C_1
+//! s_m*G + s_k*Y =? T_2 + e*C_2
+//! s_m*G + S_r*H =? T_3 + e*C_m
+//! and accepts if they all hold, otherwise it rejects
+//!
+//! Properties:
+//! Completeness: check for generic variables
+//! Special soundness: Two accepting transcripts with same initial T_1, T_2, T_3 and different challenges extracts the openings (m, r, k)
+//! zero-knowledge: From an accepting transcript, we can simulate T_1, T_2, T_3 from totally random s_m, s_r, s_k <-$ Z_q and post hoc e, proving no aditional information is leaked.
+//!
+
 use curve25519_dalek::{
     RistrettoPoint, 
     Scalar,
